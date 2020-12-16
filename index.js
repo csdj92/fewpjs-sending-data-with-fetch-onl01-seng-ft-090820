e0ae063756bdccaaf14cc29a170ef2d3593f062d
csdj92 <csdj92@gmail.com>
2020-12-16 14:50:44 -0500

    commit tree 56d27737183473de65fed775855595f2b64afe1e

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 // Add your code here
 
-function submitData(userName, emailAddress) {
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      name: userName,
-      email: emailAddress
-    })
-  }
+function submitData( name, email ) {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then(resp => resp.json())
+    .then(data => document.body.innerHTML = data["id"])
+    .catch(error => document.body.innerHTML = error.message)
 
-
-
-  return fetch("http://localhost:3000/users", configObj)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (object) {
-      let p = document.createElement('p')
-      document.body.appendChild(p)
-      p.innerHTML = object.id
-    })
-    .catch(function (error) {
-      let h1 = document.createElement('h1')
-      document.body.appendChild(h1)
-      h1.innerHTML = error.message
-    })
 }
+
